1d240eff28aa4d8b6020a3027a24c6080c712756
joel076 <joeloskarsson75@gmail.com>
2017-03-15 08:48:42 +0100

    Add files via upload

---
+++ pruank/ConsoleApplication11/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace ConsoleApplication11
+{
+    class Program
+    {
+        public static Random _random = new Random();
+
+        static void Main(string[] args)
+        {
+
+
+            Thread Mouset = new Thread(new ThreadStart(mouse));
+
+
+     
+            Mouset.Start();
+
+
+
+
+        }
+
+
+
+
+
+        public static void mouse()
+        {
+            while (true)
+            {
+                Cursor.Position = new System.Drawing.Point(Cursor.Position.X - 10, Cursor.Position.Y - 10);
+                Thread.Sleep(500);
+            }
+        }
+
+    }
+}
