0d5930d6b16e1776107c513c22df932f9db1de54
dragonpower2000 <dragonvale09@gmail.com>
2017-03-15 09:50:14 +0100

    Syncing from my PC

---
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = Console.;
+            if (isprime(x))
+            {
+                
+            }
+            
+              
+            
+        }
+
+        static bool isprime(int x)
+        {
+            bool prime = true;
+            for (int y = 2; y < x - 1; y++)
+            {
+                if (x % y == 0)
+                {
+                    prime = false;
+                }
+            }
+            if (prime)
+            {
+                return true;
+            }else
+            {
+                return false;
+            }
+        }
+    }
+}
