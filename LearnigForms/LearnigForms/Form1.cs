e874b5dfdcea3239cc23b95bbba87da31b64e944
joel076 <joeloskarsson75@gmail.com>
2017-03-15 08:41:06 +0100

    Add files via upload

---
+++ LearnigForms/LearnigForms/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LearnigForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+        
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            richTextBox1.AppendText("1");
+            
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            richTextBox1.AppendText("2");
+           
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            richTextBox1.AppendText("3");
+           
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+            richTextBox1.AppendText("4");
+           
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+            richTextBox1.AppendText("5");
+            
+        }
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+            richTextBox1.AppendText("6");
+            
+        }
+        private void button7_Click(object sender, EventArgs e)
+        {
+
+            richTextBox1.AppendText("7");
+            
+        }
+        private void button8_Click(object sender, EventArgs e)
+        {
+
+            richTextBox1.AppendText("8");
+            
+        }
+        private void button9_Click(object sender, EventArgs e)
+        {
+            
+            richTextBox1.AppendText("9");
+            
+        }
+        
+    }
+}
