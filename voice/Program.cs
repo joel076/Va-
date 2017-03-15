8359f9d7bfdf2624154b1654ac57a1844ca6384f
joel076 <joeloskarsson75@gmail.com>
2017-03-15 08:49:25 +0100

    Add files via upload

---
+++ voice/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SpeechLib;
+
+namespace ConsoleApplication14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var voice = new SpVoice();
+            voice.Speak("Enter the computer...");
+            Console.ReadLine();
+        }
+    }
+}
