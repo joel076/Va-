896f13efdde93e6c3640de7880cab4931f69e235
dragonpower2000 <dragonvale09@gmail.com>
2017-04-27 10:52:28 +0200

    Addad more comments for Joel

---
+++ PythonProgramingPlayground/PythonProgramingPlayground/PythonProgramingPlayground.py
@@ -0,0 +1,7 @@
+#You use '#' for comments, ok
+#I have no internet rn so I can't look up for loops :c
+#Seems as if <var>++ doesn't work since it isn't a number directly but an object, however <var> = <var> + 1 works
+x = 0
+while True:
+	print(x)
+	x = x +  1
