633e6f3e02efb00200c0af7150a309bdd9767e1d
joel076 <joeloskarsson75@gmail.com>
2017-04-27 10:03:11 +0200

    Update Program.cs

--- address/Program.cs
+++ address/Program.cs
@@ -42,7 +42,7 @@ namespace ConsoleApplication12
 			Person p1 = new Person("Bill","Gates","1955-","The owner of Microsoft he was also the richest person. He invented the first version of the programming language BASIC.");
 			Person p2 = new Person("Alan","Turing","1912-1954","He cracked the enigma code under ww2. He took suicide 1954.");
 			Person p3 = new Person("Ada","Lovalace","1815-1852","She made notes about Charle Babbages computer and she also wrote down what computers possibly could do in the future");
-			Person p4 = new Person("Linus","Torvalds","1969-","He made the free os Linux"); //David here, it's GNU/linux you fuckbag!, Joel here he took some shit from GNU so he could make Linux u scumbag!!
+			Person p4 = new Person("Linus","Torvalds","1969-","He made the free open sorce operating system Linux"); //David here, it's GNU/linux you fuckbag!, Joel here he took some shit from GNU so he could make Linux u scumbag!!
 			Person p5 = new Person("Charles", "Babbage", "1791-1871", "He made the first computer but it didnt work completly and the computer was mechincal it had cogs.");
 			Person p6 = new Person("William", "Shockley", "1910-1989", "Invented the transistor with John Bardeen and Walter Brattain");
 			Person p7 = new Person("John", "Bardeen", "1908-1991", "Invented the transistor with William Shockley and Walter Brattain");
