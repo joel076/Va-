7e3693b45e25ea1969ca9a6b3732b24956a082d9
dragonpower2000 <dragonvale09@gmail.com>
2017-04-28 19:32:13 +0200

    I made a botto

---
+++ DeltaBot/DeltaBot/Program.cs
@@ -0,0 +1,45 @@
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.Threading.Tasks;
+
+namespace MyBot
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+			=> new Program().MainAsync().GetAwaiter().GetResult();
+
+		public async Task MainAsync()
+		{
+			var client = new DiscordSocketClient();
+
+			client.Log += Log;
+			client.MessageReceived += MessageReceived;
+
+			string token = "MzA3NTQyMjQxMDQyNDk3NTM2.C-T0kw.26UU8gZRrfcMRUkjSsIhlGKbI6w"; 
+			await client.LoginAsync(TokenType.Bot, token);
+			await client.StartAsync();
+
+			// Block this task until the program is closed.
+			await Task.Delay(-1);
+		}
+
+		private async Task MessageReceived(SocketMessage message)
+		{
+			if (message.Author.Id != 307542241042497536)
+			{
+				if (message.Channel.Id == 274987790914027520)
+				{
+					//code here
+				}
+			}
+		}
+
+		private Task Log(LogMessage msg)
+		{
+			Console.WriteLine(msg.ToString());
+			return Task.CompletedTask;
+		}
+	}
+}
