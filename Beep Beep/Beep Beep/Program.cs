d44f01671000ad99dca2a5c4421aef2207ef717b
dragonpower2000 <dragonvale09@gmail.com>
2017-05-04 10:39:37 +0200

    ADded some stuff

---
+++ Beep Beep/Beep Beep/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows.Forms;
+using OpenGL;
+
+namespace HelloTriangle
+{
+	static class Program
+	{
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		static void Main()
+		{
+			string envDebug = Environment.GetEnvironmentVariable("DEBUG");
+			if (envDebug == "GL") {
+				KhronosApi.Log += delegate(object sender, KhronosLogEventArgs e) {
+					Console.WriteLine(e.ToString());
+				};
+				KhronosApi.LogEnabled = true;
+			}
+
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new SampleForm());
+		}
+	}
+}
---
+++ Multi thread mayhem/Multi thread mayhem/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Multi_thread_mayhem
+{
+	class Program
+	{
+
+		public static string inp;
+
+		static void Main(string[] args)
+		{
+			Thread wthread = new Thread(writer);
+			Thread rthread = new Thread(reader);
+			wthread.Start();
+			rthread.Start();
+		}
+		public static void writer()
+		{
+			for (;;)
+			{
+				Console.WriteLine(inp);
+			}
+		}
+		public static void reader()
+		{
+			for (;;)
+			{
+				inp = Console.ReadLine();
+			}
+		}
+	}
+}
